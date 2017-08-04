101a4df1c3d68831b4cec9a3a6351e158b398511
Mariana Quirino <30451006+mariquirino@users.noreply.github.com>
2017-08-04 14:20:19 -0300

    Add files via upload

--- ClienteController.java
+++ ClienteController.java
@@ -49,6 +49,7 @@ public class ClienteController {
     @FXML
     private TableColumn<Produto, Double> UserprecoColumn;
 
+    //Inicializando a TableView
     public void setTableViewUser() {
         tableViewUser.setItems(observableList);
         UserprodutoColumn.setCellValueFactory(new PropertyValueFactory<>("prod"));
@@ -59,20 +60,27 @@ public class ClienteController {
 
     public void inserirCarrinho(ArrayList<Produto> produtos) {
         inserirProdButton.setOnAction(event -> {
+            //Verificando se o produto que eu desejo comprar consta em produtos
             for (Produto produto: produtos) {
                 if (produto.getCod().equals(codigoProdTextField.getText())){
+                    //Achou o produto que desejo inserir no carrinho
                     try {
+                        //Removendo do estoque da gerencia
                         produto.saida(Integer.parseInt(qtdTextField.getText()));
+                        //Verificando se ele ja existe no carrinho
                         for (Compra compra: compras) {
                             if (compra.getCod().equals(codigoProdTextField.getText())){
+                                //Se existir no carrinho eu adiciono a qtd
                                 compra.setQtd(compra.getQtd() + Integer.parseInt(qtdTextField.getText()));
                                 tableViewUser.refresh();
                                 return;
                             }
                         }
+                        //Se nao exitir no carrinho eu insiro um novo
                         observableList.add(new Compra(produto.getNome(), produto.getCod(), produto.getPreco(), Integer.parseInt(qtdTextField.getText())));
                         tableViewUser.refresh();
                     } catch (ProdutoException e) {
+                        //Se tentar retirar mais do que existe em estoque exibir erro
                         Alert alert = new Alert(Alert.AlertType.ERROR);
                         alert.setTitle("Error Dialog");
                         alert.setHeaderText("Estoque Excedido.");
@@ -87,12 +95,14 @@ public class ClienteController {
 
     public void finalizarCompra(double[] faturamento) {
         comprarButton.setOnAction(event -> {
+            //Dialog para a pessoa confirmar a compra
             Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
             alert.setTitle("Confirmação de compra");
             alert.setHeaderText("Finalização do Pedido.");
             alert.setContentText("Tem certerza que deseja finalizar a compra?");
             Optional<ButtonType> result = alert.showAndWait();
             if (result.get() == ButtonType.OK) {
+                //Se a compra for confirmada aumentar o faturamento e limpar o carrinho para novas compras
                 for (Compra compra : compras) {
                     faturamento[0] += compra.getPreco() * compra.getQtd();
                 }
@@ -109,13 +119,29 @@ public class ClienteController {
             if (compra != null) {
                 for (Produto produto : produtos) {
                     if (produto.getCod().equals(compra.getCod())) {
+                        //Se o codigo que eu desejo remover for igual oq consta em produtos, devolver ao estoque
                         produto.setEstoque(produto.getEstoque() + compra.getQtd());
                         compra.setQtd(0);
                     }
                 }
+                //Removendo o item selecionado
                 compras.remove(compra);
                 tableViewUser.refresh();
             }
         });
     }
+
+    //Caso a pessoa feche o carrinho devolver ao estoque
+    public void removerAll(ArrayList<Produto> produtos){
+        for (Compra compra: compras) {
+            for (Produto produto : produtos) {
+                if (produto.getCod().equals(compra.getCod())) {
+                    //Se o codigo que eu desejo remover for igual oq consta em produtos, devolver ao estoque
+                    produto.setEstoque(produto.getEstoque() + compra.getQtd());
+                }
+            }
+        }
+        compras.clear();
+    }
 }
+
